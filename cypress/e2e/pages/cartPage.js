559df4c61fa33fa11823c2670b19d36805b3899c
Rafael ELoi <rafa.eloi@hotmail.com>
2024-07-18 01:08:04 -0300

    criação dos cenários do carrinho

---
+++ cypress/e2e/pages/cartPage.js
@@ -0,0 +1,30 @@
+import cartElements from "../elements/cartElements";
+
+class CartPage {
+
+    clickAddCart(){
+        cy.get(cartElements.btnAddProductPrimary()).click();
+        cy.get(cartElements.btnAddProductSecundary()).click();
+    }
+
+    clickCart(){
+        cy.get(cartElements.btnCart()).click();
+    }
+
+    cartItemValidate(){
+        cy.get(cartElements.productCartValidatePrimary()).should("be.visible");
+        cy.get(cartElements.productCartValidateSecundary()).should("be.visible");
+    }
+
+    clickRemoveCart(){
+        cy.get(cartElements.btnRemoveProductSecundary()).click();
+        cy.get(cartElements.btnRemoveProductPrimary()).click();
+    }
+
+    cartRemoveValidate(){
+        cy.get(cartElements.btnRemoveProductPrimary()).should("not.exist");
+        cy.get(cartElements.btnRemoveProductSecundary()).should("not.exist");
+    }
+}
+
+export default new CartPage();
---
+++ cypress/e2e/step_definitions/cart/cart.js
@@ -0,0 +1,41 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import loginPage from '../../pages/loginPage';
+import cartPage from '../../pages/cartPage';
+
+//Bloco de login
+Given("Acesso a tela de login", () => {
+    loginPage.accessLoginPage();
+});
+When("Preencho corretamente o usuário", () => {
+    loginPage.successUser();
+});
+When("Preencho cooretamente a senha", () => {
+    loginPage.successPassword();
+});
+When("Clico em login", () => {
+    loginPage.clickBtnLogin();
+});
+Then("Visualizo a tela de produtos", () => {
+    loginPage.successLoginValidate();
+});
+
+//Bloco do carrinho
+When("Cliclo em adicionar produtos ao carrinho", () => {
+    cartPage.clickAddCart();
+});
+
+When("Clico para ir para o carrinho", () => {
+    cartPage.clickCart();
+});
+
+Then("Vizualizo os produtos no carrinho", () => {
+    cartPage.cartItemValidate();
+});
+
+When("Clico para remover os produtos do carrinho", () => {
+    cartPage.clickRemoveCart();
+});
+
+Then("Vizualizo que os produtos foram removidos do carrinho", () => {
+    cartPage.cartRemoveValidate();
+});
