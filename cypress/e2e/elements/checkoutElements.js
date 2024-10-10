c9503a128da14a35621f27ab1afb81c44ca19bfa
Rafael ELoi <rafa.eloi@hotmail.com>
2024-10-10 00:35:40 -0300

    testando erro no report

--- cypress/e2e/elements/checkoutElements.js
+++ cypress/e2e/elements/checkoutElements.js
@@ -3,7 +3,7 @@ class CheckoutElements {
     btnCheckout = () => "#cart_contents_container > div > div.cart_footer > a.btn_action.checkout_button";
     firstNameFild = () => "#first-name";
     lastNameFild = () => "#last-name";
-    //postalCodeFild = () => "#postal-code";
+    postalCodeFild = () => "#postal-code";
     btnContinue = () => "#checkout_info_container > div > form > div.checkout_buttons > input";
     productValidatePrimary = () => "#checkout_summary_container > div > div.cart_list > div:nth-child(3)";
     productValidateSecundary = () => "#checkout_summary_container > div > div.cart_list > div:nth-child(4)";
